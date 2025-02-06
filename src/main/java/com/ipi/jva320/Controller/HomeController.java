ef50dfab4a4bfc68518df1395e1dd5cece0df821
Hugo2611 <hugo.ville26@gmail.com>
2025-02-06 16:47:56 +0100

    Afficher la page des détails d'un salarié

--- src/main/java/com/ipi/jva320/Controller/HomeController.java
+++ src/main/java/com/ipi/jva320/Controller/HomeController.java
@@ -1,10 +1,12 @@
 package com.ipi.jva320.Controller;
 
+import com.ipi.jva320.model.SalarieAideADomicile;
 import com.ipi.jva320.service.SalarieAideADomicileService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 
 @Controller
 public class HomeController {
--- src/main/java/com/ipi/jva320/Controller/SalarieController.java
+++ src/main/java/com/ipi/jva320/Controller/SalarieController.java
@@ -1,16 +1,23 @@
 package com.ipi.jva320.Controller;
 
+import com.ipi.jva320.model.SalarieAideADomicile;
+import com.ipi.jva320.service.SalarieAideADomicileService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 
 @Controller
 public class SalarieController {
 
-        @GetMapping(value = "/salaries/1")
-        public String getPatient(final ModelMap model) {
-            model.put("id", 1);
-            model.put("nom", "Jean");
-            return "p";
-        }
+    @Autowired
+    private SalarieAideADomicileService salarieAideADomicileService;
+
+    @GetMapping(value = "/salaries/{id}")
+    public String salarie(@PathVariable Long id, ModelMap model) {
+        SalarieAideADomicile salarie = salarieAideADomicileService.getSalarie(id);
+        model.put("salarie", salarie);
+        return "detail_Salarie";
+    }
 }
